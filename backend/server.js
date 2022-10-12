91e4dc8a96b546bdbe60a361b7d4b9f15f5a52e5
TREagans <thas.eagans@gmail.com>
2022-10-12 18:07:21 -0500

    project setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000 || process.env.port;
+
+app.listen(port, () => {
+  console.log(`Server listening on port...${port}`);
+});
