1a13153382028ca2646eb6733bacf6b97a09d48e
TREagans <thas.eagans@gmail.com>
2022-10-12 23:02:52 -0500

    established connection to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+
+// create an instance of express called app
 const app = express();
 
-const port = 5000 || process.env.port;
-
-app.listen(port, () => {
-  console.log(`Server listening on port...${port}`);
+app.get("/", (req, res) => {
+  res.send("Welcome to Task Manager");
 });
+
+// when we deploy app to actual server, that server provides
+// us with a port number, and we use that port from a .env file
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("[SUCCESS]: Database Connected...");
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port...${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("[FAILED]: Database Connection Failed!", error);
+  });
